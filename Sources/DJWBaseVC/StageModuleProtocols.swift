79ff91aea70ebe958d5fc33e2847a5a409b7b478
Trevor <trevor.d@beydag.com>
2020-09-13 23:57:35 +0100

    stage vc protocol temporary is added here.

---
+++ Sources/DJWBaseVC/StageModuleProtocols.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by dejaWorks on 23/03/2020.
+//
+
+import UIKit
+
+
+/// This is here temporary!
+// MARK: - VC
+public  protocol StageModuleVC {
+    var moduleTitle:String  {get set}
+    var moduleColor:UIColor {get set}
+    var moduleView:StageModuleV {get}
+}
+
+
+// MARK: - View
+public protocol StageModuleV{
+    var moduleColor:UIColor {get set}
+}
+

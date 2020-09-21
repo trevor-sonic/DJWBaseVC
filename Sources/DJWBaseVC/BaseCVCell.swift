fcef42163e30ba77a01135a05654ef106af94f76
Trevor <trevor.d@beydag.com>
2020-09-21 14:15:10 +0100

    Auto update.

---
+++ Sources/DJWBaseVC/BaseCVCell.swift
@@ -0,0 +1,35 @@
+//
+//  BaseCVCell.swift
+//  
+//
+//  Created by dejaWorks on 21/09/2020.
+//
+
+import UIKit
+
+open class BaseCVCell: UICollectionViewCell {
+    
+    
+    class var reuseID: String {return "BaseCVCell"}
+    
+    
+    // MARK: - init
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+        setConstraints()
+    }
+
+    required public init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    // MARK: - Setup
+    /// Implement UI init without super
+    open func setupUI(){}
+    
+    /// Implement UI Constraints without super
+    open func setConstraints() {}
+    
+    
+}
